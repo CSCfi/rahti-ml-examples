75df172e8fe117921efe8428cd6774e0922691cd
Mats Sjöberg <mats.sjoberg@csc.fi>
2019-10-16 09:46:52 +0300

    Added some libraries to test versions

--- wsgi.py
+++ wsgi.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 
 from flask import Flask, escape, request
-# import gensim
-# import nltk
-# import torch
+import gensim
+import nltk
 
 application = Flask(__name__)
+
 
 @application.route('/')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
+
 @application.route('/versions')
 def versions():
     import flask
-    return 'Flask version: {}\n'.format(flask.__version__)
+    return """Flask version: {}
+Gensim verison: {}
+NLTK version: {}
+""".format(flask.__version__,
+           gensim.__version__,
+           nltk.__version__)
+
 
 if __name__ == "__main__":
     application.run()
