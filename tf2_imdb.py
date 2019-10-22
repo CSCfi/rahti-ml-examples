df0077c592571eb9781ac50111452642bb2df4ad
Mats Sjöberg <mats.sjoberg@csc.fi>
2019-10-22 16:13:18 +0300

    Add new version of download_file that should work concurrently

--- tf2_imdb.py
+++ tf2_imdb.py
@@ -1,8 +1,11 @@
+import errno
+import fcntl
 import json
 import os
 import sys
 import tempfile
-import urllib
+import time
+import urllib.request
 
 import numpy as np
 import tensorflow as tf
@@ -19,17 +22,41 @@ print('Using Tensorflow version: {}, and Keras version: {}.'.format(
 
 def download_file(url):
     filename = os.path.join(tempfile.gettempdir(), os.path.basename(url))
-    if os.path.isfile(filename):
-        print('Not downloading {} as {} already present'.format(url, filename))
-        return filename
 
     dirname = os.path.dirname(filename)
     if not os.path.isdir(dirname):
         os.mkdir(dirname)
 
-    print('Downloading {} to {}'.format(url, filename))
+    # Use file locking to prevent many processes from downloading the
+    # same file at the same time ...
+    fp = open(filename, 'a')
+    was_locked = False
+    while True:
+        try:
+            fcntl.flock(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            break
+        except IOError as e:
+            if e.errno != errno.EAGAIN:
+                raise
+            else:
+                # if it was locked, we will just wait for it to become
+                # unlocked, i.e., when the download is complete
+                if not was_locked:
+                    print('Waiting, as {} is already being downloaded...'.
+                          format(url, filename))
+                was_locked = True
+                time.sleep(0.1)
 
-    urllib.request.urlretrieve(url, filename)
+    if not was_locked:
+        if os.path.getsize(filename) > 0:
+            print('Not downloading {} as {} already present'.format(
+                url, filename))
+        else:
+            print('Downloading {} to {} ...'.format(url, filename))
+            urllib.request.urlretrieve(url, filename)
+
+    # release lock and return
+    fcntl.flock(fp, fcntl.LOCK_UN)
     return filename
 
 
