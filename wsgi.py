010f9cba5c846799c6102b9294f6db79a7e5b4dc
Mats Sjöberg <mats.sjoberg@csc.fi>
2021-12-22 11:27:04 +0200

    Fixes

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,7 @@ import torch
 import transformers
 
 application = flask.Flask(__name__)
+generator = transformers.pipeline('text-generation', model='gpt2')
 
 
 @application.route('/')
@@ -12,11 +13,9 @@ def generate():
     default_text = "You didn't give me any input, so I'll just ramble on"
     text = flask.request.args.get("text", default_text)
     max_length = flask.request.args.get("max_length", 30)
+    output = generator(text, max_length=max_length, num_return_sequences=1)
 
-    generator = transformers.pipeline('text-generation', model='gpt2')
-    x = generator(text, max_length=max_length, num_return_sequences=1)
-
-    return x[0]
+    return output[0]
 
 
 @application.route('/versions')
