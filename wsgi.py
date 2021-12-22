96833c726daf3ffccd52b570a0957db5e919a7d0
Mats Sjöberg <mats.sjoberg@csc.fi>
2021-12-22 11:12:40 +0200

    Added transformers generation example with GPT-2

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
 import flask
-import os
+import torch
+import transformers
 
 application = flask.Flask(__name__)
 
 
 @application.route('/')
-def hello():
-    name = flask.request.args.get("name", "World")
+def generate():
+    default_text = "You didn't give me any input, so I'll just ramble on"
+    text = flask.request.args.get("text", default_text)
+    max_length = flask.request.args.get("max_length", 30)
+
+    generator = transformers.pipeline('text-generation', model='gpt2')
+    x = generator(text, max_length=max_length, num_return_sequences=1)
+
+    return x[0]
+
+
+@application.route('/versions')
+def versions():
     return {
-        'msg': 'Hello, {}!'.format(flask.escape(name))
+        'flask': flask.__version__,
+        'torch': torch.__version__,
+        'transformers': transformers.__version__,
     }
 
 
