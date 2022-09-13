8b609e752b29892976573d2ecaece7cb4faa50cd
Mats Sjöberg <mats.sjoberg@csc.fi>
2022-09-13 13:53:53 +0300

    Initial version of stable diffusion test

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
 
 import flask
-import os
+import tempfile
+from flask import send_file
+from torch import autocast
+from diffusers import StableDiffusionPipeline
 
 application = flask.Flask(__name__)
 
 
-@application.route('/')
-def hello():
-    name = flask.request.args.get("name", "World")
-    return {
-        'msg': 'Hello, {}!'.format(flask.escape(name))
-    }
+@application.route('/text2image')
+def text2image():
+    prompt = flask.request.args.get("prompt", "a photo of an astronaut riding a horse on mars")
+
+    # pipe = StableDiffusionPipeline.from_pretrained("/scratch/dac/stable-diffusion-v1-4")
+    # 11G - 10458MB
+    # git lfs install
+    # git clone https://huggingface.co/CompVis/stable-diffusion-v1-4
+
+    pipe = StableDiffusionPipeline.from_pretrained("/mountdata/stable-diffusion-v1-4")
+    pipe = pipe.to("cuda")
+
+    with autocast("cuda"):
+        image = pipe(prompt).images[0]
+
+    fp = tempfile.NamedTemporaryFile()
+    image.save(fp.name)
+
+    return send_file(fp.name, mimetype='image/png')
 
 
 if __name__ == "__main__":
